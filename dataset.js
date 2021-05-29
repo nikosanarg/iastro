4c54c908a1c516472ddaad0d3d76ac8dfa3c0db4
“nsandobal” <nikosanarg@gmail.com>
2021-05-29 18:40:08 -0300

    clean console messages

--- dataset.js
+++ dataset.js
@@ -1,4 +1,3 @@
-console.log("cargando dataset de entrenamiento");
 dataset_para_entrenar = [
     {input: {mas: 0.2, rad: 0.2, eda: 0.78, exc: 0.2, tem: 0.1}, output: {hab: 1}},
     {input: {mas: 0.1, rad: 0.1, eda: 0.23, exc: 0.1, tem: 0.2}, output: {hab: 1}},
