34f98ac2661a13b4b65924ca5cc9e8556a9048bf
“nsandobal” <nikosanarg@gmail.com>
2021-05-29 18:39:09 -0300

    IA DONE

---
+++ dataset.js
@@ -0,0 +1,7 @@
+console.log("cargando dataset de entrenamiento");
+dataset_para_entrenar = [
+    {input: {mas: 0.2, rad: 0.2, eda: 0.78, exc: 0.2, tem: 0.1}, output: {hab: 1}},
+    {input: {mas: 0.1, rad: 0.1, eda: 0.23, exc: 0.1, tem: 0.2}, output: {hab: 1}},
+    {input: {mas: 0.8, rad: 0.5, eda: 0.69, exc: 0.95, tem: 0.9}, output: {hab: 0}},
+    {input: {mas: 0.9, rad: 0.95, eda: 0.51, exc: 0.5, tem: 0.8}, output: {hab: 0}}
+]
